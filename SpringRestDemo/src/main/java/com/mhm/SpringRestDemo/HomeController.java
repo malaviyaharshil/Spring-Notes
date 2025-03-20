0d2b9ba88cc45957a754ced06001f7a1969b0b6e
harshil0218 <malaviyah.218@gmail.com>
2025-03-20 23:51:52 +0530

    added some changes

--- SpringRestDemo/src/main/java/com/mhm/SpringRestDemo/HomeController.java
+++ SpringRestDemo/src/main/java/com/mhm/SpringRestDemo/HomeController.java
@@ -1,13 +1,18 @@
 package com.mhm.SpringRestDemo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String greet(){
         return "Hello World";
+    }
+    @GetMapping("/greet")
+    public String greet2(){
+        return "Welcome to my first Spring Boot Deployed Application";
     }
 }
